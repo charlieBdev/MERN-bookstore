5eb8b4bd4bb5e85f9f29d31a759e3aedfd0408fc
bigspambot <67501435+bigspambot@users.noreply.github.com>
2023-09-26 19:31:09 +0100

    post book

--- backend/config.js
+++ backend/config.js
@@ -1,3 +1,3 @@
-export const PORT = 9001
+export const PORT = 5555
 
 export const mongoDBURL = 'mongodb+srv://charlieBdev:1akcU8oSq6fVOxmU@mern-bookstore.wnpkc5j.mongodb.net/books-collection?retryWrites=true&w=majority&appName=AtlasApp'
