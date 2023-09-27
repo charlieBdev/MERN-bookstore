66d7ccb0fce48ed47e78245e1d5351976873bb3a
bigspambot <67501435+bigspambot@users.noreply.github.com>
2023-09-27 12:27:45 +0100

    backend done and fe basic setup started

--- backend/index.js
+++ backend/index.js
@@ -1,38 +1,34 @@
 import express from "express"
 import { PORT, mongoDBURL } from "./config.js"
 import mongoose from "mongoose"
-import { Book } from "./models/bookModel.js"
+import booksRoute from './routes/booksRoute.js'
+import cors from 'cors'
 
 const app = express()
 
+// Middleware for parsing request.body
 app.use(express.json())
+
+// Middleware efor handling CORS
+// Option 1 - Allows all origins with default of cors(*)
+// app.use(cors())
+// Option 2 - Allow custom origins
+app.use(cors({
+  origin: 'http://localhost:3000',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type']
+}))
 
 app.get('/', (request, response) => {
   console.log(request)
   return response.status(200).send('Welcome to the MERN Bookstore')
 })
 
-app.post('/books', async (request, response) => {
-  try {
-    if (
-      !request.body.title ||
-      !request.body.author ||
-      !request.body.publishYear
-    ) {
-      return response.status(400).send({
-        message: 'Send all required fields: title, author, publishYear'
-      })
-    }
-    const newBook = {
-      title: request.body.title,
-      author: request.body.author,
-      publishYear: request.body.publishYear,
-    }
-    const book = await Book.create(newBook)
-    return response.status(201).send(book)
-  } catch (error) {
-    console.log(error.message)
-  }
+app.use('/api/books', booksRoute)
+
+// ALL: Not found (I added)
+app.all("*", (_, res) => {
+  res.status(404).send({ msg: 'Not found' })
 })
 
 mongoose
---
+++ frontend/src/App.jsx
@@ -0,0 +1,7 @@
+function App() {
+  return (
+    <div className="bg-black text-white">App</div>
+  )
+}
+
+export default App
