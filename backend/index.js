8948402005ddb013d61cdfa370845faf22c8b489
bigspambot <67501435+bigspambot@users.noreply.github.com>
2023-09-28 17:12:15 +0100

    change cors back

--- backend/index.js
+++ backend/index.js
@@ -14,13 +14,14 @@ app.use(express.json())
 
 // Middleware efor handling CORS
 // Option 1 - Allows all origins with default of cors(*)
-// app.use(cors())
+app.use(cors())
 // Option 2 - Allow custom origins - PREFERRED
-app.use(cors({
-  origin: 'https://mern-bookstore-nwrh.onrender.com',
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  allowedHeaders: ['Content-Type']
-}))
+// app.use(cors({
+//   // needs to be front end dep
+//   origin: 'https://mern-bookstore-nwrh.onrender.com',
+//   methods: ['GET', 'POST', 'PUT', 'DELETE'],
+//   allowedHeaders: ['Content-Type']
+// }))
 
 app.get('/', (request, response) => {
   console.log(request)
